0f94ece967f2139a8c558fcf41d3606b8485a489
Abanoub Hendy <abanoubhendi@gmail.com>
2021-02-05 12:32:28 +0200

    second commit

--- src/client/index.js
+++ src/client/index.js
@@ -6,8 +6,15 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 import './styles/resets.scss'
+import './images/natural-language-processing-1110x831-1.png'
+import './images/LogoMeaningCloud650x264.png'
 
 console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
+export {
+    checkForName,
+    handleSubmit
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(`Your API key is ${process.env.API_KEY}`);
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-var textapi = MeaningCloud({
-    application_key: process.env.API_KEY
-  });
+const apiKey = process.env.API_KEY;
+var https = require('follow-redirects').https;
+var fs = require('fs');
+console.log(`Your API key is ${process.env.API_KEY}`);
+
 
 const app = express()
 
@@ -20,12 +21,35 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+var options = {
+    'method': 'POST',
+    'hostname': 'api.meaningcloud.com',
+    'path': '/sentiment-2.1?key=186c9dd70631cf3585378167bad6b588&lang=<lang>&txt=<text>&model=<model>',
+    'headers': {
+    },
+    'maxRedirects': 20
+};
+var req = https.request(options, function (res) {
+    var chunks = [];
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+    res.on("end", function (chunk) {
+        var body = Buffer.concat(chunks);
+        console.log(body.toString());
+    });
+    res.on("error", function (error) {
+        console.error(error);
+    });
+});
+req.end();
 
 
