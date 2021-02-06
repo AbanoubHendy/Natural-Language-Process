e39b5fbacca5a5b737e0c5316131ca65538b5ade
Abanoub Hendy <abanoubhendi@gmail.com>
2021-02-06 15:02:10 +0200

    fourth commit

--- src/client/index.js
+++ src/client/index.js
@@ -7,8 +7,8 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 import './styles/resets.scss'
-import img from './images/LogoMeaningCloud650x264.png';
-import img from './images/natural-language-processing-1110x831-1.png';
+import img1 from './images/LogoMeaningCloud650x264.png'
+import img2 from './images/natural-language-processing-1110x831-1.png'
 
 console.log(checkForName);
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,13 +3,12 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
     optimization: {
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        minimizer: [new OptimizeCSSAssetsPlugin({})],
     },
     module: {
         rules: [
@@ -19,8 +18,8 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.scss$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+                test: /\.(sa|sc|c)ss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
