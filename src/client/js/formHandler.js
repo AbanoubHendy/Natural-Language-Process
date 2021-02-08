5523e5c6d7a40c03f5016f50fe1ad0e3a1e9fc10
Abanoub Hendy <abanoubhendi@gmail.com>
2021-02-08 17:59:05 +0200

    8th commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,5 @@
+import { response } from "express"
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -9,8 +11,35 @@ function handleSubmit(event) {
     fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+        postData();
+        UpdateUI();
+    });
 }
+
+const postData = async (url = '' , formText = {})=>{
+    console.log(data);
+    const res = await fetch('http://localhost:8081/test', {
+        method: 'POST',
+        credentials: 'same-origin' ,
+        headers: {
+            'Content-Type' : 'application/json' ,
+        },
+        body: JSON.stringify(formText),
+    });
+}
+
+const UpdateUI = async ()=> {
+    const request = await fetch('/test');
+    try{
+        const alldata = await request.json();
+        document.getElementById('Irony').innerHTML=alldata.irony;
+        document.getElementById('Agreement').innerHTML=alldata.agreement;
+        document.getElementById('Confidence').innerHTML=alldata.confidence;
+        document.getElementById('Subjectivity').innerHTML=alldata.subjectivity;
+        document.getElementById('Score_tag').innerHTML=alldata.score_tag;
+    }catch(error) {
+        console.log('There Is An Error' , error)
+    }
+};
 
 export { handleSubmit }
